9988254142c3c0134b79cce70dbe3bd49b9a1853
Andersson <m-mannen@live.se>
2018-11-12 15:49:49 +0100

    added some headers

--- server.js
+++ server.js
@@ -3,5 +3,11 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.get("/hej", (req, res) => {
+
+    res.render("card.ejs", {
+        theme: "theme",
+    });
+});
 
 app.listen(3000, () => console.log("listening on port 3000"));
